0b518826b18532a7ebc2f5398186f68018b4d55b
JyotiProkash <cse0718@gmail.com>
2022-04-03 19:23:58 +0600

    added dataprovider page

---
+++ src/main/java/com/hybridframework/qa/dataproviders/MyDataProvider.java
@@ -0,0 +1,16 @@
+package com.hybridframework.qa.dataproviders;
+
+import com.hybridframework.qa.objects.Product;
+import com.hybridframework.qa.utils.JacksonUtils;
+import org.testng.annotations.DataProvider;
+
+import java.io.IOException;
+
+public class MyDataProvider {
+
+/*    @DataProvider(name = "getFeaturedProducts", parallel = false)
+    public Object[] getFeaturedProducts() throws IOException {
+        return JacksonUtils.deserializeJson("products.json", Product[].class);
+    }*/
+}
+
