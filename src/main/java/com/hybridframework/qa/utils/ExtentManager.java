dae73816aea2b1b2fc36975faab374619afc2c70
JyotiProkash <cse0718@gmail.com>
2022-04-17 13:10:00 +0600

    updated ExtentManager file.

--- src/main/java/com/hybridframework/qa/utils/ExtentManager.java
+++ src/main/java/com/hybridframework/qa/utils/ExtentManager.java
@@ -1,11 +1,11 @@
 package com.hybridframework.qa.utils;
-
 import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.hybridframework.qa.helper.resource.ResourceHelper;
+import org.testng.annotations.Parameters;
 
+import java.io.File;
 import java.io.IOException;
-import java.util.Date;
 
 /**
  * Created by Jyoti 22/04/2020
@@ -13,37 +13,49 @@ import java.util.Date;
 public class ExtentManager
   {
       private static ExtentReports extent;
-      public static ExtentReports getInstance() throws IOException
-        {
-            if(extent==null)
-              {
-                  return createInstance();
-              }
+      public static ExtentReports getInstance(String browser) {
+            if(extent==null) {
+                return createInstance(browser);
+            }
+
             else
               {
                   return extent;
               }
         }
-      private static ExtentReports createInstance() throws IOException {
-            Date d=new Date();
+
+      private static ExtentReports createInstance(String browser){
+            //Date d=new Date();
             //String fileName="AutomationReport_"+d.toString().replace(":","_").replace(" ","_")+".html";
             String fileName="ExtentReport.html";
             String directory=ResourceHelper.getResourcePath("src/test/output/TestReport/");
-            //new File(directory).mkdirs();
+            new File(directory).mkdirs();
             String path=directory+fileName;
-            ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(path);
-            /*htmlReporter.config().setDocumentTitle("Selenium Automation");
-            htmlReporter.config().setTheme(Theme.DARK);
+/*          ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(path);
+            htmlReporter.config().setDocumentTitle("Selenium Automation");
+            htmlReporter.config().setTheme(Theme.STANDARD);
             htmlReporter.config().setEncoding("utf-8");
-            htmlReporter.config().setReportName("Automation Execution Report");*/
-            //htmlReporter.loadXMLConfig(ResourceHelper.getResourcePath("src/main/resources/ConfigFiles/extent-config.xml"));
-            htmlReporter.loadXMLConfig(ResourceHelper.getResourcePath("extent-config.xml"));
-            extent=new ExtentReports();
-            extent.attachReporter(htmlReporter);
+            htmlReporter.config().setReportName("Automation Execution Report");
+            htmlReporter.loadXMLConfig("extent-config.xml");*/
+            final File CONF = new File("extent-config.xml");
+            ExtentSparkReporter spark = new ExtentSparkReporter(path);
+/*          spark.config().setDocumentTitle("Selenium Automation");
+            spark.config().setTheme(Theme.STANDARD);
+            spark.config().setEncoding("utf-8");
+            spark.config().setReportName("Automation Execution Report");*/
+          try {
+              spark.loadXMLConfig(CONF);
+          } catch (IOException e) {
+              e.printStackTrace();
+          }
+
+
+          extent=new ExtentReports();
             extent.setSystemInfo("Application Name", "ExtentReport");
             extent.setSystemInfo("Platform", System.getProperty("os.name"));
-            extent.setSystemInfo("Environment", "QA");
-            extent.setSystemInfo("Browser Name", ConfigLoader.getInstance().browserName());
+            extent.setSystemInfo("Environment", "STG");
+            extent.setSystemInfo("Browser Name", browser);
+            extent.attachReporter(spark);
             return extent;
 
         }
